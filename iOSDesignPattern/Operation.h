04a239cdb9ab934d0b7fb7e01927ca659f4b71bc
xp_mac <xp_mac@xp-macdeMini.lan>
2016-04-08 15:26:46 +0800

    添加父类

---
+++ iOSDesignPattern/Operation.h
@@ -0,0 +1,13 @@
+//
+//  Operation.h
+//  iOSDesignPattern
+//
+//  Created by xp_mac on 16/4/8.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Operation : NSObject
+
+@end
---
+++ iOSDesignPattern/Operation.m
@@ -0,0 +1,13 @@
+//
+//  Operation.m
+//  iOSDesignPattern
+//
+//  Created by xp_mac on 16/4/8.
+//  Copyright © 2016年 xp_mac. All rights reserved.
+//
+
+#import "Operation.h"
+
+@implementation Operation
+
+@end
